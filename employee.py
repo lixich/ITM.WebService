12bd9fb4da2ba48d88e34e1efe1d10041ad72eeb
Chusovliankin Aleksei <lixich@mail.ru>
2018-03-04 17:23:21 +0500

    add Image

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, abort, request
 from db import update_record, create_record
-import os
+import Images
 
 app_employee = Blueprint('employee', __name__)
 employee_set = [
@@ -14,7 +14,7 @@ employee_set = [
         "TesterSkill": 35,
         "TesterCapacity": 200,
         "Salary": 300,
-        "Image" :  os.getcwd() + "\\Images\\Aleksei.jpg"
+        "Image" :  "https://www.marvellouscutouts.co.uk/wp-content/uploads/2017/10/Beckham.jpg"
     },
     {
         "Id": 2,
@@ -25,7 +25,8 @@ employee_set = [
         "DeveloperCapacity": 200,
         "TesterSkill": 90,
         "TesterCapacity": 100,
-        "Salary": 200
+        "Salary": 200,
+        "Image" :  "https://s-media-cache-ak0.pinimg.com/originals/e7/8c/1c/e78c1c6f7471c201ea17f3a5b43ab275.jpg"
     },
     {
         "Id": 3,
@@ -36,7 +37,8 @@ employee_set = [
         "DeveloperCapacity": 200,
         "TesterSkill": 75,
         "TesterCapacity": 150,
-        "Salary": 400
+        "Salary": 400,
+        "Image" :  "http://www.mens-hairstyle.com/wp-content/uploads/2016/12/Classic-Undercut.jpg"
     },
         {
         "Id": 4,
@@ -47,7 +49,8 @@ employee_set = [
         "DeveloperCapacity": 100,
         "TesterSkill": 70,
         "TesterCapacity": 150,
-        "Salary": 500
+        "Salary": 500,
+        "Image" :  "https://knownetworth.com/uploads/thumb/seth-macfarlane-net-worth-350-350.jpeg"
     },
     {
         "Id": 5,
@@ -58,7 +61,8 @@ employee_set = [
         "DeveloperCapacity": 100,
         "TesterSkill": 55,
         "TesterCapacity": 200,
-        "Salary": 100
+        "Salary": 100,
+         "Image" :  "http://www.eshorthairstyles.com/wp-content/uploads/2017/04/19.Celebrity-Short-Hair-2016.jpg"
     }
 ]
 employee_class = {
