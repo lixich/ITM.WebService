5d5286c53dc2ac17f1bab0e4d460a3ec1688314b
Chusovliankin Aleksei <lixich@mail.ru>
2018-03-09 19:37:34 +0500

    fix null

--- db.py
+++ db.py
@@ -4,15 +4,9 @@ def create_record(_class, _request, _record):
     for _field in _class.keys():
         # _record is empty, only Ids
         if _field not in _record.keys():
-            if _field in request.json and request.json[_field] is None:
-                _record[_field] = None
-            else:
-                _record[_field] = _class[_field](request.json.get(_field, _class[_field]()))
+            _record[_field] = request.json.get(_field, _class[_field]())
 
 def update_record(_class, _request, _record):
     for _field in _class.keys():
         if _field in request.json:
-            if request.json[_field] is None:
-                _record[_field] = None
-            else:
-                _record[_field] = _class[_field](request.json[_field])
+            _record[_field] = request.json[_field]
